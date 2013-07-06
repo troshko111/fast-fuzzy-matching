f305ba12e28c3727b9960fc1a49c24af277d3382
Taras Roshko <taras.roshko@gmail.com>
2013-07-06 15:27:24 +0300

    PoC

---
+++ src/FFM/FFM/DamerauLevenshteinDistance.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FFM
+{
+    public interface IDistance
+    {
+        int Calculate(string x, string y);
+    }
+
+    public class DamerauLevenshteinDistance : IDistance
+    {
+        public int Calculate(string x, string y)
+        {
+            return Distance(x, y);
+        }
+
+        // Damerau–Levenshtein distance algorithm and code 
+        // from http://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance
+
+        public static int Distance(string source, string target)
+        {
+            if (string.IsNullOrEmpty(source))
+                return string.IsNullOrEmpty(target) ? 0 : target.Length;
+
+            if (string.IsNullOrEmpty(target))
+                return source.Length;
+
+            var score = new int[source.Length + 2, target.Length + 2];
+
+            var INF = source.Length + target.Length;
+            score[0, 0] = INF;
+            for (var i = 0; i <= source.Length; i++)
+            {
+                score[i + 1, 1] = i;
+                score[i + 1, 0] = INF;
+            }
+            for (var j = 0; j <= target.Length; j++)
+            {
+                score[1, j + 1] = j;
+                score[0, j + 1] = INF;
+            }
+
+            var sd = new SortedDictionary<char, int>();
+            foreach (var letter in (source + target))
+            {
+                if (!sd.ContainsKey(letter))
+                    sd.Add(letter, 0);
+            }
+
+            for (var i = 1; i <= source.Length; i++)
+            {
+                var DB = 0;
+                for (var j = 1; j <= target.Length; j++)
+                {
+                    var i1 = sd[target[j - 1]];
+                    var j1 = DB;
+
+                    if (source[i - 1] == target[j - 1])
+                    {
+                        score[i + 1, j + 1] = score[i, j];
+                        DB = j;
+                    }
+                    else
+                    {
+                        score[i + 1, j + 1] = Math.Min(score[i, j], Math.Min(score[i + 1, j], score[i, j + 1])) + 1;
+                    }
+
+                    score[i + 1, j + 1] = Math.Min(score[i + 1, j + 1], score[i1, j1] + (i - i1 - 1) + 1 + (j - j1 - 1));
+                }
+
+                sd[source[i - 1]] = i;
+            }
+
+            return score[source.Length + 1, target.Length + 1];
+        }
+    }
+}
---
+++ src/FFM/FFM/IDistance.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FFM
+{
+    public class BKTree
+    {
+        public BKTreeNode Root;
+
+        private readonly IDistance _distance;
+
+        public BKTree(IDistance distance)
+        {
+            _distance = distance;
+        }
+
+        public void Add(BKTreeNode node)
+        {
+            if (Root == null)
+            {
+                Root = node;
+                return;
+            }
+
+            AddChild(Root, node);
+        }
+
+        private void AddChild(BKTreeNode node, BKTreeNode newNode)
+        {
+            var d = _distance.Calculate(node.Data, newNode.Data);
+            if (node.Children.All(child => child.DistanceToParent != d))
+            {
+                newNode.DistanceToParent = d;
+                node.Children.Add(newNode);
+                return;
+            }
+
+            var corresponding = node.Children.Single(c => c.DistanceToParent == d);
+            AddChild(corresponding, newNode);
+        }
+
+        public List<string> Matches(string query, int maxDistance)
+        {
+            var results = new List<string>();
+            Matches(Root, query, maxDistance, results);
+            return results;
+        }
+
+        private void Matches(BKTreeNode node, string query, int maxDistance, List<string> matches)
+        {
+            var d = _distance.Calculate(node.Data, query);
+            if (d <= maxDistance)
+                matches.Add(node.Data);
+
+            var start = d - maxDistance > 0 ? d - maxDistance : 0;
+            var end = d + maxDistance;
+            for (var i = start; i <= end; i++)
+            {
+                var child = node.Children.SingleOrDefault(c => c.DistanceToParent == i);
+                if (child != null)
+                    Matches(child, query, maxDistance, matches);
+            }
+        }
+    }
+
+    public class BKTreeNode
+    {
+        public string Data;
+        public int DistanceToParent;
+        public List<BKTreeNode> Children;
+
+        public BKTreeNode(string data)
+        {
+            Data = data;
+            Children = new List<BKTreeNode>();
+        }
+    }
+}
